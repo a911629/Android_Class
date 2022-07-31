59ebbb74e93e3fa79a8087dc345f425399c5980f
calvinchen <calvinchen@msi.com>
2022-07-31 23:03:47 +0800

    Android code for class NO.13 1. Add one method bmi to calculate BMI value by reading data weight and height from the picture made by activity_main.xml.  Then set button listener doing by designed page.

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+    }
+
+    public void bmi(View view) {
+        EditText edWeight = findViewById (R.id.ed_weight);
+        EditText edHeight = findViewById (R.id.ed_height);
+        String w = edWeight.getText ().toString ();
+        String h = edHeight.getText ().toString ();
+        float weight = Float.parseFloat (w);
+        float height = Float.parseFloat (h);
+        float bmi = weight / (height * height);
+        Log.d ("MainActivity", "BMI : " + bmi);
     }
 }
