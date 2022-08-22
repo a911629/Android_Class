7bee36b2ec6ab80540bc7f7bf6bfb42844282b04
calvinchen <calvinchen@msi.com>
2022-08-22 09:56:29 +0800

    Android code for class NO.31 1. Design a page Finance which can accounting info. 2. Design a database use SQL_lite which using to record data and output data to finance page.

---
+++ ATM/app/src/main/java/com/calvin/atm/AddActivity.java
@@ -0,0 +1,46 @@
+package com.calvin.atm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+public class AddActivity extends AppCompatActivity {
+
+    private EditText edDate;
+    private EditText edInfo;
+    private EditText edAmount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        edDate = findViewById(R.id.ed_date);
+        edInfo = findViewById(R.id.ed_info);
+        edAmount = findViewById(R.id.ed_amount);
+
+    }
+
+    public void add(View view) {
+        String date = edDate.getText().toString();
+        String info = edInfo.getText().toString();
+        int amount = Integer.parseInt(edAmount.getText().toString());
+//        ExpenseHelper helper = new ExpenseHelper(this, "atm", null, 1); 因為ExpenseHelper有新增建構子，這個舊的建構方式改為private了，改為以下方式。
+        ExpenseHelper helper = new ExpenseHelper(this);
+        ContentValues values = new ContentValues();
+        values.put("cdate", date);
+        values.put("info", info);
+        values.put("amount", amount);
+        long id = helper.getWritableDatabase()
+                .insert("expense", null, values);
+        if(id > -1) {
+            Toast.makeText(this, "新增成功", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "新增失敗", Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ ATM/app/src/main/java/com/calvin/atm/FinanceActivity.java
@@ -0,0 +1,119 @@
+package com.calvin.atm;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.calvin.atm.databinding.ActivityFinanceBinding;
+
+public class FinanceActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityFinanceBinding binding;
+    private ExpenseAdapter adapter;
+    private RecyclerView recyclerView;
+    private ExpenseHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityFinanceBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_finance2);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FinanceActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        helper = new ExpenseHelper( this);
+        Cursor cursor = helper.getReadableDatabase()
+                .query("expense",
+                        null,null,null,
+                        null, null ,null);
+
+        adapter = new ExpenseAdapter(cursor);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Cursor cursor = helper.getReadableDatabase()
+                .query("expense", null,null,null,
+                        null, null ,null);
+        adapter = new ExpenseAdapter(cursor);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseHolder> {
+        Cursor cursor;
+        public ExpenseAdapter(Cursor cursor) {
+            this.cursor = cursor;
+        }
+
+        @NonNull
+        @Override
+        public ExpenseHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.expense_item, parent, false);
+            return new ExpenseHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ExpenseHolder holder, int position) {
+            cursor.moveToPosition(position);
+            @SuppressLint("Range") String date = cursor.getString(cursor.getColumnIndex("cdate"));
+            @SuppressLint("Range") String info = cursor.getString(cursor.getColumnIndex("info"));
+            @SuppressLint("Range") int amount = cursor.getInt(cursor.getColumnIndex("amount"));
+            holder.dateText.setText(date);
+            holder.infoText.setText(info);
+            holder.amountText.setText(String.valueOf(amount));
+        }
+
+        @Override
+        public int getItemCount() {
+            return cursor.getCount();
+        }
+
+        public class ExpenseHolder extends RecyclerView.ViewHolder{
+            TextView dateText;
+            TextView infoText;
+            TextView amountText;
+            public ExpenseHolder(@NonNull View itemView) {
+                super(itemView);
+                dateText = itemView.findViewById(R.id.item_date);
+                infoText = itemView.findViewById(R.id.item_info);
+                amountText = itemView.findViewById(R.id.item_amount);
+            }
+        }
+    }
+}
