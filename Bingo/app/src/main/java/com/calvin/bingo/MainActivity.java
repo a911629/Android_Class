aa09251f8bb23e34892c5b3bc2b7f52c20f0f5b2
calvinchen <calvinchen@msi.com>
2022-09-15 12:30:25 +0800

    Android Class
    Build a new project Bingo. But can not execute correctly.
    Admin access still got problem.

--- Bingo/app/src/main/java/com/calvin/bingo/MainActivity.java
+++ Bingo/app/src/main/java/com/calvin/bingo/MainActivity.java
@@ -8,39 +8,61 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.calvin.bingo.databinding.ActivityMainBinding;
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.material.snackbar.Snackbar;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener, ValueEventListener {
+public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener, ValueEventListener, View.OnClickListener {
 
     private static final int RC_SING_IN = 100;
     private static final String TAG = MainActivity.class.getSimpleName();
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
     private FirebaseAuth auth;
+//    FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
+//        @Override
+//        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+//            if(currentUser == null) {
+//                new AuthUI.SignInIntentBuilder().
+//            }
+//        }
+//    };
     private FirebaseUser user;
     private TextView nickText;
+    int[] avatars = { R.drawable.avatar_0, R.drawable.avatar_1, R.drawable.avatar_2,R.drawable.avatar_3,R.drawable.avatar_4};
+    private ImageView avatar;
     private Member member;
+    private Group avatarGroup;
+    private RecyclerView recycleView;
+    private FirebaseRecyclerAdapter<Room, RoomHolder> adapter;
 
 
     @Override
@@ -49,12 +71,10 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+//        setContentView(R.layout.activity_main);
         findViews();
 
-
         setSupportActionBar(binding.toolbar);
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
@@ -62,8 +82,28 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                final EditText titleEdit = new EditText(MainActivity.this);
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Room title")
+                        .setView(titleEdit)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String roomTitle = titleEdit.getText().toString();
+                                DatabaseReference roomRef = FirebaseDatabase.getInstance().getReference("rooms").push();
+                                Room room = new Room(roomTitle, member);
+                                roomRef.setValue(room);
+                                String key = roomRef.getKey();
+                                Log.d(TAG, "onClick: Room key: " + key);
+                                roomRef.child("key").setValue(key);
+                                //TODO: enter game room
+                                Intent bingo = new Intent(MainActivity.this, BingoActivity.class);
+                                bingo.putExtra("ROOM KEY", key);
+                                bingo.putExtra("IS_CREATOR", true);
+                                startActivity(bingo);
+                            }
+                        }).setNeutralButton("Cancel", null)
+                        .show();
             }
         });
         auth = FirebaseAuth.getInstance();
@@ -71,7 +111,70 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
 
     private void findViews() {
         nickText = findViewById(R.id.nickname);
+        avatar = findViewById(R.id.avatar);
+        avatarGroup = findViewById(R.id.group_avatars);
+        recycleView = findViewById(R.id.recycler);
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                avatarGroup.setVisibility(avatarGroup.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+                avatarGroup.setVisibility(avatarGroup.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+            }
+        });
+        findViewById(R.id.avatar_0).setOnClickListener(this);
+        findViewById(R.id.avatar_1).setOnClickListener(this);
+        findViewById(R.id.avatar_2).setOnClickListener(this);
+        findViewById(R.id.avatar_3).setOnClickListener(this);
+        findViewById(R.id.avatar_4).setOnClickListener(this);
+
+        //recyclerView
+        recycleView.setHasFixedSize(true);
+        recycleView.setLayoutManager(new LinearLayoutManager(this));
+        Query query = FirebaseDatabase.getInstance().getReference("rooms").orderByKey();
+        FirebaseRecyclerOptions<Room> options = new FirebaseRecyclerOptions.Builder<Room>()
+                .setQuery(query, Room.class).build();
+        adapter = new FirebaseRecyclerAdapter<Room, RoomHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull RoomHolder holder, int position, @NonNull Room model) {
+                holder.title.setText(model.getTitle());
+                holder.image.setImageResource(avatars[model.getCreator().getAvatar()]);
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent bingo = new Intent(MainActivity.this, BingoActivity.class);
+                        bingo.putExtra("ROOM KEY", model.getKey());
+                        bingo.putExtra("IS_CREATOR", false);
+                        startActivity(bingo);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public RoomHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = getLayoutInflater().inflate(R.layout.item_room, parent, false);
+                return new RoomHolder(view);
+            }
+        };
     }
+
+    @Override
+    public void onClick(View v) {
+        avatarGroup.setVisibility(avatarGroup.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+    }
+
+
+    class RoomHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        ImageView image;
+
+        public RoomHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.room_image);
+            title = itemView.findViewById(R.id.room_title);
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -149,7 +252,7 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
         member = snapshot.getValue(Member.class);
         Log.d(TAG, "onDataChange: " + member.getUid());
         if(member.getNickName() == null) {
-            EditText nickEdit = new EditText(this);
+            final EditText nickEdit = new EditText(this);
             new AlertDialog.Builder(this)
                     .setTitle("Nickname")
                     .setView(nickEdit)
